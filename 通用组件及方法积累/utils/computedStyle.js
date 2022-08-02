2ac4363aea87e1b90bca35de42885a5be9bcfa2f
MooreLxr <1052777399@qq.com>
2022-08-02 17:18:08 +0800

    Delete computedStyle.js

--- 通用组件及方法积累/utils/computedStyle.js
+++
@@ -1,8 +0,0 @@
-const computedStyle = (eleDom) => {
-  if (window.getComputedStyle) {
-    return getComputedStyle(eleDom, null)
-  } else {
-    return eleDom.currentStyle // 兼容IE的写法
-  }
-}
-export default computedStyle
