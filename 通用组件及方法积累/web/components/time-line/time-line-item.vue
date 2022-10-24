bc27fa563f8efc52e6e38d1213638ad8c927edcc
MooreLxr <1052777399@qq.com>
2022-10-24 09:29:11 +0800

    Update time-line-item.vue

--- 通用组件及方法积累/web/components/time-line/time-line-item.vue
+++ 通用组件及方法积累/web/components/time-line/time-line-item.vue
@@ -1,28 +1,20 @@
 <template>
-  <div class="time-line-item"
-       @mouseenter="hoverActive"
-       @mouseleave="hoverClose">
-    <div class="time-line-item-date" v-if="$slots.date">
-      <slot name="date"></slot>
-    </div>
-    <div class="time-line-item-dot">
-      <!-- 自定义点 -->
-      <slot name="dot" v-if="$slots.dot"></slot>
-      <!-- 默认点样式 -->
-      <div v-else style="width:100%;height:100%;">
-        <span class="dot"></span>
-        <img :src="`${assets}/img/common/error.png`"
-            alt=""
-            class="del-icon"
-            v-if="deleteEnable"
-            v-show="delIconVisible"
-            @click="emitDel">
+  <div class="time-line-item">
+    <slot name="prefix"></slot>
+    <div class="time-line-item__wrap">
+      <div class="time-line-item__line" ref="line"></div>
+      <div class="time-line-item__date" v-if="$slots.date" ref="dateDom">
+        <slot name="date"></slot>
+      </div>
+      <div class="time-line-item__dot" ref="dotDom">
+        <slot name="dot" v-if="$slots.dot"></slot>
+        <span class="dot" v-else></span>
+      </div>
+      <div class="time-line-item__content">
+        <slot name="content"></slot>
       </div>
     </div>
-
-    <div class="time-line-item-tooltip">
-      <slot name="content"></slot>
-    </div>
+    <slot name="suffix"></slot>
   </div>
 </template>
 
@@ -32,94 +24,92 @@
  * @description 时间轴的每项
  */
 export default {
-  props: {
-    permissions: Array,//权限['Delete']
-    default:[]
-  },
   data () {
-    return {
-      deleteEnable: false,//是否可删除
-      delIconVisible: false,//删除按钮显示标志位
-    }
+    return {}
   },
-  created () {
-    this.deleteEnable = this.permissions && this.permissions.indexOf('Delete') >= 0 ? true : false
+  created () {},
+  mounted() {
+    this.setLinePos()
   },
   methods: {
-    hoverActive () {
-      this.delIconVisible = true
-    },
-    hoverClose () {
-      this.delIconVisible = false
-    },
-    emitDel () {
-      this.$emit("del")
+    setLinePos() {
+      const { line, dateDom, dotDom } = this.$refs
+      let w = 0
+      if (dateDom) {
+        const style = getComputedStyle(dateDom)
+        w += dateDom.clientWidth
+        w += +style['margin-left'].replace('px', '')
+        w += +style['margin-right'].replace('px', '')
+      }
+      if (dotDom) {
+        w += (dotDom.clientWidth / 2)
+        w += +getComputedStyle(dotDom)['margin-left'].replace('px', '')
+      }
+      line.style.left = w + 'px'
     }
   }
 }
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .time-line-item {
   width: 100%;
-  position: relative;
+  transition: all .3s;
+}
+.time-line-item__wrap {
   display: flex;
   align-items: center;
-  margin-bottom: 10px;
   cursor: pointer;
-  .time-line-item-date {
-    width: 60px;
-    flex: 0 0 60px;
-    text-align: right;
-    font-size: 14px;
-    color: #000;
-  }
-  .time-line-item-dot {
+  position: relative;
+}
+.time-line-item__line {
+  width: 0.5px;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  position: absolute;
+  bottom: 0;
+}
+.time-line-item__date {
+  width: 70px;
+  text-align: right;
+  color: rgba(0, 0, 0, 0.6);
+  font-size: 16px;
+  display: inline-block;
+}
+.time-line-item__dot {
+  margin: 0 5px;
+  position: relative;
+  .dot {
     height: 12px;
     width: 12px;
-    margin: 0 5px;
-    flex: 0 0 12px;
-    position: relative;
-    .dot {
-      width: 100%;
-      height: 100%;
-      display: inline-block;
-      border-radius: 50%;
-      background-color: #666;
-    }
-    .del-icon {
-      width: 16px;
-      height: 16px;
-      position: absolute;
-      top: -2px;
-      left: -2px;
-      background-color: #fff;
-    }
+    display: inline-block;
+    border-radius: 50%;
+    background-color: #666;
   }
-  .time-line-item-tooltip {
-    flex: 1;
-    padding: 12px;
-    box-sizing: border-box;
-    border-radius: 5px;
-    background: #fff;
-    border: 1px solid #ebeef5;
-    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-    position: relative;
-    margin-left: 12px;
-    &:after {
-      content: "";
-      position: absolute;
-      left: -20px;
-      top: 50%;
-      width: 0;
-      height: 0;
-      border: 10px solid #fff;
-      border-top-color: transparent;
-      border-bottom-color: transparent;
-      border-left-color: transparent;
-      transform: translateY(-50%);
-    }
+}
+.time-line-item__content {
+  flex: 1;
+  padding: 12px;
+  box-sizing: border-box;
+  border-radius: 5px;
+  background: #fff;
+  border: 1px solid #ebeef5;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  position: relative;
+  margin-left: 12px;
+  &:after {
+    content: "";
+    position: absolute;
+    left: -20px;
+    top: 50%;
+    width: 0;
+    height: 0;
+    border: 10px solid #fff;
+    border-top-color: transparent;
+    border-bottom-color: transparent;
+    border-left-color: transparent;
+    transform: translateY(-50%);
   }
 }
 </style>
