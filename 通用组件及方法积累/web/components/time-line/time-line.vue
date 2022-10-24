6c347ee4283094d237123a42b01dc18ecfd4f119
MooreLxr <1052777399@qq.com>
2022-10-24 09:28:43 +0800

    update time-line

--- 通用组件及方法积累/web/components/time-line/time-line.vue
+++ 通用组件及方法积累/web/components/time-line/time-line.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="time-line">
-    <div class="time-line-line"
-         :style="`left:${showDate?'74px':'14px'}`"></div>
     <slot></slot>
   </div>
 </template>
@@ -13,9 +11,11 @@
  * 调用示例：
  * import timeLine from "@/components/time-line/time-line"
  * import timeLineItem from "@/components/time-line/time-line-item"
- * <time-line :showDate="true">
-      <time-line-item v-for="(item,i) in historyData"
-                      :key="i">
+ * <time-line>
+      <time-line-item
+        v-for="(item,i) in historyData"
+        :key="i"
+      >
         <div slot="date">{{item.startingTime | timeFormat}}</div>
         <div slot="content">
           <span>{{item.disasterName}}</span>
@@ -24,13 +24,6 @@
     </time-line>
  */
 export default {
-  props: {
-    //是否展示左侧的日期
-    showDate: {
-      type: Boolean,
-      default: false
-    }
-  },
   data () {
     return {}
   },
@@ -40,18 +33,5 @@ export default {
 
 </script>
 
-<style lang="scss">
-.time-line {
-  width: 100%;
-  position: relative;
-  padding: 5px;
-  box-sizing: border-box;
-  .time-line-line {
-    position: absolute;
-    top: 22px;
-    bottom: 13px;
-    width: 4px;
-    background-color: #ddd;
-  }
-}
+<style lang="scss" scoped>
 </style>
